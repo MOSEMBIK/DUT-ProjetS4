f914cd3876e6a58d1577854d481d61b27475dd99
Bastien <bastien.rimbert@laposte.net>
2022-02-07 14:06:18 +0100

    Commentaires à jour du Main.cpp & Modification de la tâche de lancement qui visait le mauvais emplacement

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,31 @@
 
 using namespace std;
 
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "An Error has occured: %s\n", description);
+}
+
 int main(int argc, char **argv)
 {
 	GLFWwindow* window;
 
-	/* Initialize the library */
+	// Initialise la librairie GLFW
 	if (!glfwInit())
 		return -1;
+		
+	// Définit la fonction appelée lors d'erreur
+	glfwSetErrorCallback(error_callback);
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(1280, 720, "Hello World !", NULL, NULL);
+	// Créer une fenêtre dans un contexte OpenGL
+	window = glfwCreateWindow(720, 720, "Hello World !", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
 		return -1;
 	}
 
-	/* Make the window's context current */
+	// Met le contexte de la fenêtre en contexte principal
 	glfwMakeContextCurrent(window);
 
 	if (glewInit() != GLEW_OK)
@@ -33,18 +41,16 @@ int main(int argc, char **argv)
 
 	Quad quad;
 
-	/* Loop until the user closes the window */
+	// Tant que la fenêtre ne doit pas être fermer (Alt-F4 ou click sur la croix par exemple)
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
 		quad.Draw();
 
-		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
-		/* Poll for and process events */
+		// Récupérer les events tels que les clicks, les touches claviers etc...
 		glfwPollEvents();
 	}
 
