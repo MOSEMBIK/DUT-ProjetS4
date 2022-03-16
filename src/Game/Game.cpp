659621d4764d64ce369f623a350645e8e99ef070
Stoupy51 <stoupy51@gmail.com>
2022-03-16 23:05:54 +0100

    Ajustement du menu multijoueur

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -445,13 +445,13 @@ void Game::setState(GameState state)
 		unsigned char i = 1;
 		for (ServerInfo server : servers) { i++;
 			buttons.push_back(new Button(mainWindow, vec2(0.0f, -i*79.0f), vec2(0.5f, 1.0f), vec2(DEFAULT_WINDOW_W/1.5, 60), (char *)"assets/button.png", vec3(1.0f)*0.75f, vec3(0.75f, 0.75f, 0.5f)*0.75f, vec3(0.5f)*0.75f));
-			labels.push_back(new Label(mainWindow, vec2(30.0f, -i*79.0f), vec2(0.225f, 1.0f), server.m_serverOwner				, 24, bomberFont, ALIGN_LEFT | ALIGN_MIDDLE));
-			labels.push_back(new Label(mainWindow, vec2(20.0f, -i*79.0f), vec2(0.185f, 1.0f), server.m_players					, 24, bomberFont, ALIGN_LEFT | ALIGN_MIDDLE, vec3(0.69f)));
+			labels.push_back(new Label(mainWindow, vec2(-300.0f, -i*79.0f), vec2(0.5f, 1.0f), server.m_serverOwner				, 24, bomberFont, ALIGN_LEFT | ALIGN_MIDDLE));
+			labels.push_back(new Label(mainWindow, vec2(-400.0f, -i*79.0f), vec2(0.5f, 1.0f), server.m_players					, 24, bomberFont, ALIGN_LEFT | ALIGN_MIDDLE, vec3(0.69f)));
 			vec3 pingColor = vec3(1.0f, 0.0f, 0.0f);
 			if (server.m_ping < 32) { pingColor = vec3(0.0f, 1.0f, 0.0f); }
 			else if (server.m_ping < 100) { pingColor = vec3(1.0f, 1.0f, 0.0f); }
-			labels.push_back(new Label(mainWindow, vec2(0.0f, -i*79.0f), vec2(0.63f, 1.0f), to_string(server.m_ping)+" ms"		, 24, bomberFont, ALIGN_CENTER | ALIGN_MIDDLE, pingColor));
-			labels.push_back(new Label(mainWindow, vec2(50.0f, -i*79.0f), vec2(0.70f, 1.0f), "Join server"						, 24, bomberFont, ALIGN_CENTER | ALIGN_MIDDLE));
+			labels.push_back(new Label(mainWindow, vec2(200.0f, -i*79.0f), vec2(0.5f, 1.0f), to_string(server.m_ping)+" ms"		, 24, bomberFont, ALIGN_RIGHT | ALIGN_MIDDLE, pingColor));
+			labels.push_back(new Label(mainWindow, vec2(400.0f, -i*79.0f), vec2(0.5f, 1.0f), "Join server"						, 24, bomberFont, ALIGN_RIGHT | ALIGN_MIDDLE));
 			buttons[i]->setOnClickCallback([server]() {
 				cerr << "Rejoindre " << server.m_ip << endl;
 			});
